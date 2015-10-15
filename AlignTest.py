5efe21d248a1f3295d4377bdaea4db2e4480631a
ddhmed <799390224@qq.com>
2015-10-15 15:11:58 +0800

    Test
    Test

---
+++ AlignTest.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 17:35:48 2015
+
+@author: DFW
+"""
+from alignment.sequence import Sequence
+from alignment.vocabulary import Vocabulary
+from alignment.sequencealigner import SimpleScoring, GlobalSequenceAligner
+
+# Create sequences to be aligned.
+a = Sequence('ATCGAGGGATAGCGAAGA')
+b = Sequence('AAGTCGGGGTACGGCAAC')
+
+# Create a vocabulary and encode the sequences.
+v = Vocabulary()
+aEncoded = v.encodeSequence(a)
+bEncoded = v.encodeSequence(b)
+
+# Create a scoring and align the sequences using global aligner.
+scoring = SimpleScoring(2, -1)
+aligner = GlobalSequenceAligner(scoring, -2)
+score, encodeds = aligner.align(aEncoded, bEncoded, backtrace=True)
+
+# Iterate over optimal alignments and print them.
+for encoded in encodeds:
+    alignment = v.decodeSequenceAlignment(encoded)
+    print alignment
+    print 'Alignment score:', alignment.score
+    print 'Percent identity:', alignment.percentIdentity()
+
